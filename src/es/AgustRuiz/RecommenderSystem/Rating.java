af00ad70204944021cf3283f4f7311c5339b2b06
Agustín Ruiz Linares <agustinruizlinares@gmail.com>
2015-05-29 22:09:56 +0200

    Rating.java and RatingDAO.java

---
+++ src/es/AgustRuiz/RecommenderSystem/Rating.java
@@ -0,0 +1,95 @@
+package es.AgustRuiz.RecommenderSystem;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Agustin Ruiz Linares <arl00029@red.ujaen.es>
+ */
+public class Rating {
+    
+    /*** VARIABLES ***/
+    
+    /// User Id
+    private Integer iduser = null; 
+    
+    /// Item Id
+    private Integer iditem = null;
+    
+    /// Rating [1,5]
+    private Float rating = null;
+    
+    /// Timestamp
+    private Timestamp timestamp = null;
+    
+    /*** METHODS ***/
+    
+    /**
+     * Constructor
+     */
+    public Rating(){
+        this.InitializeVariables();
+    }
+
+    /**
+     * Get iduser of the rating
+     * @return iduser of the rating
+     */
+    public Integer getIduser() {
+        return iduser;
+    }
+
+    /**
+     * 
+     * @param iduser 
+     */
+    public void setIduser(Integer iduser) {
+        this.iduser = iduser;
+    }
+
+    public Integer getIditem() {
+        return iditem;
+    }
+
+    public void setIditem(Integer iditem) {
+        this.iditem = iditem;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+    
+    /**
+     * Initialize variables
+     */
+    private void InitializeVariables() {
+        this.iduser = -1;
+        this.iditem = -1;
+        this.rating = 0f;
+        this.timestamp = new Timestamp(0);
+    }
+    
+    /**
+     * Get the Rating information in a string
+     * @return String with Rating information
+     */
+    public String toString(){
+        String s = "[RATE] Iduser: " + this.iduser +
+                ", Iditem: " + this.iditem +
+                ", Rating: " + this.rating +
+                ", Timestamp: " + this.timestamp;
+        return s;
+    }
+}
---
+++ src/es/AgustRuiz/RecommenderSystem/RatingDAO.java
@@ -0,0 +1,127 @@
+package es.AgustRuiz.RecommenderSystem;
+
+import java.util.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Agustin Ruiz Linares <arl00029@red.ujaen.es>
+ */
+public class RatingDAO {
+
+    /**
+     * Get a rating of an User for an Item
+     *
+     * @param iduser User Id
+     * @param iditem Item Id
+     * @return Rating if exist or null otherwise
+     */
+    public static Rating get(int iduser, int iditem) {
+        Rating rating = null;
+        try {
+            PreparedStatement query = DbConnection.getConnection().prepareStatement("SELECT * FROM ratings WHERE iduser =" + iduser + " AND iditem=" + iditem);
+            ResultSet rs = query.executeQuery();
+            if (rs.next()) {
+                rating = RatingDAO.fillRatings(rs);
+            }
+            rs.close();
+            query.close();
+        } catch (Exception e) {
+            System.err.println("Can't get list of ratings from database. " + e);
+        }
+        return rating;
+    }
+
+    /**
+     * Get all a user ratings
+     *
+     * @param iduser User id
+     * @return List of ratings of the User
+     */
+    public static HashMap<Integer, Rating> getRatingsOfUser(int iduser) {
+        HashMap<Integer, Rating> ratingMap = new HashMap();
+        try {
+            PreparedStatement query = DbConnection.getConnection().prepareStatement("SELECT * FROM ratings WHERE iduser =" + iduser);
+            ResultSet rs = query.executeQuery();
+            while (rs.next()) {
+                Rating rating = RatingDAO.fillRatings(rs);
+                ratingMap.put(rating.getIditem(), rating);
+            }
+            rs.close();
+            query.close();
+        } catch (Exception e) {
+            System.err.println("Can't get list of ratings from database. " + e);
+        }
+        return ratingMap;
+    }
+
+    /**
+     * Gets the list of Items from database
+     *
+     * @return List of items from database
+     */
+    public static List<Rating> getList() {
+        List<Rating> ratingsList = new ArrayList();
+        try {
+            PreparedStatement query = DbConnection.getConnection().prepareStatement("SELECT * FROM ratings");
+            ResultSet rs = query.executeQuery();
+            while (rs.next()) {
+                ratingsList.add(RatingDAO.fillRatings(rs));
+            }
+            rs.close();
+            query.close();
+        } catch (Exception e) {
+            System.err.println("Can't get list of ratings from database. " + e);
+        }
+        return ratingsList;
+    }
+
+    /**
+     * Fills a Rating from a ResultSet
+     *
+     * @param rs ResultSet
+     * @return Rating filled from rs
+     * @throws SQLException
+     */
+    private static Rating fillRatings(ResultSet rs) throws SQLException {
+        Rating rating = new Rating();
+        rating.setIduser(Integer.parseInt(rs.getString("iduser")));
+        rating.setIditem(Integer.parseInt(rs.getString("iditem")));
+        rating.setRating(Float.parseFloat(rs.getString("rating")));
+        rating.setTimestamp(TimestampParser(rs.getString("timestamp")));
+        return rating;
+    }
+
+    /**
+     * Timestamp parser
+     *
+     * @param timestampString String timestamp
+     * @return timestampString converted to a Timestamp
+     */
+    private static Timestamp TimestampParser(String timestampString) {
+        Timestamp timestamp = null;
+        if (timestampString == null) {
+            timestamp = new Timestamp(0);
+        } else {
+            try {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                Date parsedDate = dateFormat.parse(timestampString);
+                timestamp = new Timestamp(parsedDate.getTime());
+            } catch (ParseException ex) {
+                timestamp = new Timestamp(0);
+            }
+        }
+        return timestamp;
+    }
+}
