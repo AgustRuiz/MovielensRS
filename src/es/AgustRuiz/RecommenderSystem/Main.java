0fa1c1b0c9850e3f9fd1a1d0ae355b1a067eb49b
Agustín Ruiz Linares <agustinruizlinares@gmail.com>
2015-06-04 13:48:42 +0200

    Refactor Recommender.java and Main.java

--- src/es/AgustRuiz/RecommenderSystem/Main.java
+++ src/es/AgustRuiz/RecommenderSystem/Main.java
@@ -26,86 +26,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        long time;
 
         User activeUser = UserDAO.get(USER_TEST);
 
-        /* * * NEIGHBORHOOD * * */
-        System.out.println("Building neighbourhood...");
-        time = System.currentTimeMillis();
-
-        HashMap<Double, User> neighborhoodSimilarityUser = Recommender.KNN(activeUser, K_VALUE);
-        HashMap<Integer, Double> avgRatingsUsers = new HashMap();
-        for (User user : neighborhoodSimilarityUser.values()) {
-            int iduser = user.getIduser();
-            avgRatingsUsers.put(iduser, RatingDAO.avgRatings(iduser));
-        }
+        HashMap<Item, Double> recommendations = Recommender.makeRecomendations(activeUser, K_VALUE);
         
-        System.out.println("Neighbourhood builded in " + (System.currentTimeMillis() - time) + " ms!\n");
-        /* * * /NEIGHBORHOOD * * */
-
-        /* * * RATINGS ACTIVE USER * * */
-        System.out.println("Getting ratings for active user...");
-        time = System.currentTimeMillis();
-
-        HashMap<Integer, Rating> ratingsActiveUser = RatingDAO.getRatingsOfUser(USER_TEST);
-        double avgRatingsActiveUser = RatingDAO.avgRatings(USER_TEST);
-
-        System.out.println("Done in " + (System.currentTimeMillis() - time) + " ms!\n");
-        /* * * /RATINGS ACTIVE USER * * */
-
-        /* * * RATING MATRIX * * */
-        System.out.println("Calculating similarity and rating matrix...");
-        time = System.currentTimeMillis();
-
-        HashMap<Integer, Double> similarityMap = new HashMap(); // HashMap<ItemId, Similarity>
-        RatingMatrix ratingMatrix = new RatingMatrix();
-        for (Entry<Double, User> entrySimilarUser : neighborhoodSimilarityUser.entrySet()) {
-            int similarUserId = entrySimilarUser.getValue().getIduser();
-            similarityMap.put(similarUserId, entrySimilarUser.getKey());
-            for (Entry<Integer, Rating> entryIditemRating : RatingDAO.getRatingsOfUser(similarUserId).entrySet()) {
-                ratingMatrix.set(entrySimilarUser.getValue().getIduser(), entryIditemRating.getKey(), entryIditemRating.getValue().getRating());
-            }
-        }
-
-        System.out.println("Similarity and rating matrix builded in " + (System.currentTimeMillis() - time) + " ms!\n");
-        /* * * /RATING MATRIX * * */
-
-        /* * * RECOMMENDATIONS * * */
-        System.out.println("Calculating recommendations...");
-        time = System.currentTimeMillis();
-
-        HashMap<Item, Double> recommendations = new HashMap();
-        HashMap<Integer, Item> itemHashMap = ItemDAO.getList();
-
-        for (Item currentItem : itemHashMap.values()) {
-            int currentItemId = currentItem.getIditem();
-            if (!ratingsActiveUser.containsKey(currentItemId)) {
-
-                double dividend, divisor, similarity;
-                dividend = divisor = similarity = 0;
-
-                for (int currentUserId : similarityMap.keySet()) {
-                    Double currentRating = ratingMatrix.get(currentUserId, currentItemId);
-                    if (currentRating == null) {
-                        continue;
-                    }
-                    similarity = similarityMap.get(currentUserId);
-                    dividend += similarity * (currentRating - avgRatingsUsers.get(currentUserId));
-                    divisor += abs(similarity);
-                }
-                if (divisor != 0) {
-                    double predictedRating = avgRatingsActiveUser + (dividend / divisor);
-                    recommendations.put(currentItem, predictedRating);
-                    System.err.println("");
-                }
-
-            }
-        }
-
-        System.out.println("Finished in " + (System.currentTimeMillis() - time) + " ms!\n");
-        /* * * /RECOMMENDATIONS * * */
-
         /* * * RESULTS * * */
         System.out.println("RESULTS:");
         for (Entry<Item, Double> entry : recommendations.entrySet()) {
--- src/es/AgustRuiz/RecommenderSystem/Recommender.java
+++ src/es/AgustRuiz/RecommenderSystem/Recommender.java
@@ -5,6 +5,7 @@
  */
 package es.AgustRuiz.RecommenderSystem;
 
+import static java.lang.Math.abs;
 import static java.lang.Math.sqrt;
 import java.util.Collections;
 import java.util.HashMap;
@@ -91,6 +92,88 @@ public class Recommender {
             }
         }
         return neighbor;
+    }
+    
+    public static HashMap<Item, Double> makeRecomendations(User activeUser, int neigborhoodSize){
+        long time;
+        int activeUserId = activeUser.getIduser();
+        
+        /* * * NEIGHBORHOOD * * */
+        System.out.println("Building neighbourhood...");
+        time = System.currentTimeMillis();
+
+        HashMap<Double, User> neighborhoodSimilarityUser = Recommender.KNN(activeUser, neigborhoodSize);
+        HashMap<Integer, Double> avgRatingsUsers = new HashMap();
+        for (User user : neighborhoodSimilarityUser.values()) {
+            int iduser = user.getIduser();
+            avgRatingsUsers.put(iduser, RatingDAO.avgRatings(iduser));
+        }
+        
+        System.out.println("Neighbourhood builded in " + (System.currentTimeMillis() - time) + " ms!\n");
+        /* * * /NEIGHBORHOOD * * */
+
+        /* * * RATINGS ACTIVE USER * * */
+        System.out.println("Getting ratings for active user...");
+        time = System.currentTimeMillis();
+
+        HashMap<Integer, Rating> ratingsActiveUser = RatingDAO.getRatingsOfUser(activeUserId);
+        double avgRatingsActiveUser = RatingDAO.avgRatings(activeUserId);
+
+        System.out.println("Done in " + (System.currentTimeMillis() - time) + " ms!\n");
+        /* * * /RATINGS ACTIVE USER * * */
+
+        /* * * RATING MATRIX * * */
+        System.out.println("Calculating similarity and rating matrix...");
+        time = System.currentTimeMillis();
+
+        HashMap<Integer, Double> similarityMap = new HashMap(); // HashMap<ItemId, Similarity>
+        RatingMatrix ratingMatrix = new RatingMatrix();
+        for (Entry<Double, User> entrySimilarUser : neighborhoodSimilarityUser.entrySet()) {
+            int similarUserId = entrySimilarUser.getValue().getIduser();
+            similarityMap.put(similarUserId, entrySimilarUser.getKey());
+            for (Entry<Integer, Rating> entryIditemRating : RatingDAO.getRatingsOfUser(similarUserId).entrySet()) {
+                ratingMatrix.set(entrySimilarUser.getValue().getIduser(), entryIditemRating.getKey(), entryIditemRating.getValue().getRating());
+            }
+        }
+
+        System.out.println("Similarity and rating matrix builded in " + (System.currentTimeMillis() - time) + " ms!\n");
+        /* * * /RATING MATRIX * * */
+
+        /* * * RECOMMENDATIONS * * */
+        System.out.println("Calculating recommendations...");
+        time = System.currentTimeMillis();
+
+        HashMap<Item, Double> recommendations = new HashMap();
+        HashMap<Integer, Item> itemHashMap = ItemDAO.getList();
+
+        for (Item currentItem : itemHashMap.values()) {
+            int currentItemId = currentItem.getIditem();
+            if (!ratingsActiveUser.containsKey(currentItemId)) {
+
+                double dividend, divisor, similarity;
+                dividend = divisor = similarity = 0;
+
+                for (int currentUserId : similarityMap.keySet()) {
+                    Double currentRating = ratingMatrix.get(currentUserId, currentItemId);
+                    if (currentRating == null) {
+                        continue;
+                    }
+                    similarity = similarityMap.get(currentUserId);
+                    dividend += similarity * (currentRating - avgRatingsUsers.get(currentUserId));
+                    divisor += abs(similarity);
+                }
+                if (divisor != 0) {
+                    double predictedRating = avgRatingsActiveUser + (dividend / divisor);
+                    recommendations.put(currentItem, predictedRating);
+                }
+
+            }
+        }
+
+        System.out.println("Finished in " + (System.currentTimeMillis() - time) + " ms!\n");
+        /* * * /RECOMMENDATIONS * * */
+        
+        return recommendations;
     }
 
     /**
